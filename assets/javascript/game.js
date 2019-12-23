927d926b59bde2ca015ff01ce9ccaa122a994bb4
Rachael Andrew <r.andrew.dev@gmail.com>
2019-12-22 23:10:47 -0600

    linked and tested jquery library

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,8 +11,45 @@ function initializeGame () {
     behp = 100;
     bjbhp = 150;
     bjhp = 180;
-    
+
 }
+
+$(document).ready(function() {
+    console.log( "ready!" );
+});
+
+
+// Inital screen set up, hard coded in HTML
+
+// Part One.
+// When ANY character is chosen, other characters move to the ENEMIES stage and their background turn red.
+// The CHOOSE AN ENEMY stage flag is dynamically created when ANY character is chosen. 
+// Whichever character is chosen moves down and is labeled "your character"
+// The above three lines should happen inside the character click event. 
+
+// Part two.
+// Once an enemy is clicked, the enemy moves down into the staging area and the 
+// other enemies backgrounds turn back to white.
+// The attack button is dynamically created. 
+
+
+// Part three.
+// When the attack button is clicked, text-based attacks are made. 
+// The enemies attack amount should always be the same, whereas the player's
+// character should gain in attack HP each time. 
+
+// If the enemy reaches 0 HP, the player has won. a message stating "You've won!
+// Choose another enemy to fight" should display. Another enemy is selected and the patten repeats
+// until the player has defeated all of the enemies. 
+
+// IF however the player reaches 0 HP before any of the enemies, it is game over. 
+// A message alerting the player they have lost should appear, and a reset game button should display. 
+
+// When reset game is selected, initializeGame function should run. 
+
+
+
+
 
 
 

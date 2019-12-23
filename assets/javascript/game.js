03a58aabed4b5b7ebc84200a4a00cca49e1308d0
Rachael Andrew <r.andrew.dev@gmail.com>
2019-12-22 22:42:27 -0600

    styled title, added names and HP to characters, began declaring variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+var byhp = 120;
+var behp = 100;
+var bjbhp = 150;
+var bjhp = 180;
+
+
+
+
+function initializeGame () {
+    byhp = 120;
+    behp = 100;
+    bjbhp = 150;
+    bjhp = 180;
+    
+}
+
+
+
+
